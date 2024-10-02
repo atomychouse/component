7a60710d3b4fecfadc5a89418b53ed60760e8cd6
ARIOS48 <68341+ARIOS48@users.noreply.github.ford.com>
2024-10-02 08:59:33 -0600

    development environment 101

---
+++ app/__init__.py
@@ -0,0 +1,80 @@
+from flask import (
+    Flask, 
+    jsonify, 
+    render_template,
+    request)
+
+from flask_sqlalchemy import SQLAlchemy
+import os
+import json
+import pandas as pd
+import mimetypes
+
+ROOT_DIR = os.path.abspath(os.getcwd())
+
+class Pratto:
+    def list_files(self, dirname=None):
+        if os.path.exists(dirname):
+            return [
+            f"{dirname}/{filename}" for filename in os.listdir(f"{dirname}")
+            if "excel" in mimetypes.guess_type(filename)[0]
+            ]
+        return []
+
+pratto_actions = Pratto()
+app = Flask(
+    __name__, 
+    template_folder = "templates",
+    static_folder = "statics"
+    
+    )
+
+DEF_CONFIG = ['DID', 'DIAG', 'DE']
+
+@app.route('/')
+def initial():
+
+    return render_template("index.html")
+
+@app.route('/load_fts/', methods = ['POST'])
+def load_fts():
+    applies = [
+        "MY25 S832 Gold",
+        "MY25 CX720 ",
+        "MY24 S832 Gold",
+        ]
+    return render_template("lista.html", applies=applies)
+
+
+@app.route('/fts/', methods = ['POST','GET'])
+def list_fts():
+
+    total = []
+
+    for i in range(30):
+        po =[
+        ("Selectable Drive Mode_Cfg DE05",0x1),
+        ("Brand_Cfg [DE0B]","0x0"),
+        ("SelDrvMde_D2Rq",0x0),
+        ("SelDrvMde_D2_Rq",0x0),
+        ("SelDrvMdeMsgTxt_D_Rq",0x1),
+        ("SelDrvMdePos01_B_Avail",0x1),
+        ("SelDrvMdePos01_D_Stat",0x0),
+         ("ActvDrvMde_D2_Stat",i)
+          ]
+        total.append(po)
+    return render_template("fts.html", total=total)
+
+
+
+@app.route('/runner/<int:idtest>', methods = ['GET'])
+def fun_tc(idtest):
+
+    return jsonify({'run':True})
+
+
+
+
+    assert False, df
+    assert False, dir(data)
+    return jsonify({'operation pratto':ROOT_DIR})
